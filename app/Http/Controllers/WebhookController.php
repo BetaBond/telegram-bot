7ab14727959285fe22eafa011fbc9a250d7ea31c
BetaBond <bond.ryo.beta@gmail.com>
2023-04-26 17:32:33 +0800

    fix: init

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Validation\Rule;
 use Telegram\Bot\Api;
+use Telegram\Bot\Exceptions\TelegramSDKException;
 
 /**
  * 消息控制器
@@ -35,18 +37,72 @@ class WebhookController
      * 消息处理中心
      *
      * @param  Request  $request
-     * @return array
+     * @return bool
+     * @throws TelegramSDKException
      */
-    public function message(Request $request): array
+    public function message(Request $request): bool
     {
         $requestParams = $request::validate([
             'update_id' => ['required', 'integer'],
             'message' => ['required', 'array'],
         ]);
         
-        Log::info(json_encode($requestParams['message'], JSON_UNESCAPED_UNICODE));
+        $request::validate([
+            'message_id' => ['required', 'integer'],
+            'from' => ['required', 'array'],
+            'chat' => ['required', 'array'],
+            'date' => ['required', 'integer'],
+            'text' => ['required', 'string'],
+        ], $requestParams['message']);
         
-        return [];
+        $request::validate([
+            'id' => ['required', 'integer'],
+            'is_bot' => ['required', 'boolean'],
+            'first_name' => ['required', 'string'],
+            'username' => ['required', 'string'],
+            'language_code' => ['required', 'string'],
+        ], $requestParams['message']['from']);
+        
+        // 排除机器人消息
+        if ($requestParams['message']['from']['is_bot'] !== false) {
+            return false;
+        }
+        
+        // 群消息和私聊消息分流处理
+        
+        $request::validate([
+            'id' => ['required', 'integer'],
+            'type' => ['required', 'string', Rule::in(['group', 'private'])],
+        ], $requestParams['message']['chat']);
+        
+        $chatType = $requestParams['message']['chat']['type'];
+        
+        if ($chatType === 'group') {
+            $request::validate([
+                'title' => ['required', 'string'],
+                'all_members_are_administrators' => ['required', 'boolean'],
+            ], $requestParams['message']['chat']);
+        }
+        
+        if ($chatType === 'private') {
+            $request::validate([
+                'first_name' => ['required', 'string'],
+                'username' => ['required', 'string'],
+            ], $requestParams['message']['chat']);
+        }
+        
+        $chatId = $requestParams['message']['chat']['id'];
+        $textMessage = $requestParams['message']['text'];
+        $formId = $requestParams['form']['id'];
+        $formFirstName = $requestParams['form']['first_name'];
+        $formUserName = $requestParams['form']['user_name'];
+        
+        $this->telegram->sendMessage([
+            'chat_id' => $chatId,
+            'text' => "接收到消息: $textMessage, 由$formFirstName(@$formUserName|$formId)发送 "
+        ]);
+        
+        return true;
     }
     
 }
