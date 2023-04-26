5b36054114745ac338089719a3f7e84760b2f373
BetaBond <bond.ryo.beta@gmail.com>
2023-04-26 17:08:02 +0800

    fix: init

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Request;
+use Telegram\Bot\Api;
 
 /**
  * 消息控制器
@@ -13,9 +14,37 @@ use Illuminate\Support\Facades\Request;
 class WebhookController
 {
     
+    /**
+     * 实例类
+     *
+     * @var Api
+     */
+    protected Api $telegram;
+    
+    /**
+     * 创建一个新的控制器实例
+     *
+     * @param  Api  $telegram
+     */
+    public function __construct(Api $telegram)
+    {
+        $this->telegram = $telegram;
+    }
+    
+    /**
+     * 消息处理中心
+     *
+     * @param  Request  $request
+     * @return array
+     */
     public function message(Request $request): array
     {
-        Log::info(json_encode($request::all(), JSON_PARTIAL_OUTPUT_ON_ERROR));
+        $requestParams = $request::validate([
+            'update_id' => ['required', 'integer'],
+            'message' => ['required', 'array'],
+        ]);
+        
+        Log::info(json_encode($requestParams['message'], JSON_UNESCAPED_UNICODE));
         
         return [];
     }
