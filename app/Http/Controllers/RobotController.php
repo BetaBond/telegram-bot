7e5900833dc13a21b3e576116ef0a9f36c8be654
BetaBond <bond.ryo.beta@gmail.com>
2023-04-26 16:41:48 +0800

    fix: init

--- app/Http/Controllers/RobotController.php
+++ app/Http/Controllers/RobotController.php
@@ -35,7 +35,7 @@ class RobotController
     public function webhook(): array
     {
         $response = Telegram::setWebhook([
-            'url' => 'https://southwan.cn/telegram/webhook'
+            'url' => 'https://robot.southwan.cn/telegram/webhook'
         ]);
         
         return [$response];
