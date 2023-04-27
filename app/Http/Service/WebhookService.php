70e0a5358396e24d131a7df4c9cb1e03315206ac
BetaBond <bond.ryo.beta@gmail.com>
2023-04-27 20:25:10 +0800

    fix: init

--- app/Http/Service/WebhookService.php
+++ app/Http/Service/WebhookService.php
@@ -144,9 +144,9 @@ class WebhookService
                     $difference = $money / $exchangeRate;
                 }
                 
-                $money = str_replace('.', '\\.', (string) $money);
-                $difference = str_replace('.', '\\.', (string) $difference);
-                $exchangeRate = str_replace('.', '\\.', (string) $exchangeRate);
+                $money = str_replace('.', "\\.", (string) $money);
+                $difference = str_replace('.', "\\.", (string) $difference);
+                $exchangeRate = str_replace('.', "\\.", (string) $exchangeRate);
                 
                 $message[] = "`\\[$date\\]`  \\|  $money/$exchangeRate=$difference";
             }
