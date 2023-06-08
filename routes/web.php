f9970a6a04fa64f4fd1f7ee9119d6d7fce48703d
BetaBond <bond.ryo.beta@gmail.com>
2023-06-08 17:14:21 +0800

    fix: init

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,21 +10,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::any('/', function () {
-    $response = Http::get(
-        'https://www.okx.com/v3/c2c/tradingOrders/mostUsedPaymentAndBestPriceAds',
-        [
-            't' => time().'000',
-            'cryptoCurrency' => 'USDT',
-            'fiatCurrency' => 'CNY',
-            'side' => 'buy',
-        ]
-    );
-    
-    if (!$response->successful()) {
-        return '获取失败！';
-    }
-    
-    return $response->json();
-});
