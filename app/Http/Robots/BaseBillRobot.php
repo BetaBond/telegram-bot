91bdd24761162ac3826e5afb42fefe9991ef3433
BetaBond <bond.ryo.beta@gmail.com>
2023-06-09 17:30:42 +0800

    fix: init

--- app/Http/Robots/BaseBillRobot.php
+++ app/Http/Robots/BaseBillRobot.php
@@ -508,12 +508,12 @@ class BaseBillRobot
         
         $messages = [
             '*机器人信息：*',
-            "名称 : [`$robot->firstName`]",
-            "唯一标识 : [`$robot->id`]",
-            "账号 : [`$robot->username`]",
-            '是否允许加入群组 : '.$robot->canJoinGroups ? '允许' : '不允许',
-            '是否可以阅读所有的群组信息 : '.$robot->canReadAllGroupMessages ? '允许' : '不允许',
-            '支持内联查询 : '.$robot->supportsInlineQueries ? '支持' : '不支持',
+            "名称  :  [`$robot->firstName`]",
+            "唯一标识  :  [`$robot->id`]",
+            "账号  :  [`$robot->username`]",
+            '是否允许加入群组  :  '.($robot->canJoinGroups ? '允许' : '不允许'),
+            '是否可以阅读所有的群组信息  :  '.($robot->canReadAllGroupMessages ? '允许' : '不允许'),
+            '支持内联查询  :  '.($robot->supportsInlineQueries ? '支持' : '不支持'),
         ];
         
         return implode("\n", $messages);
