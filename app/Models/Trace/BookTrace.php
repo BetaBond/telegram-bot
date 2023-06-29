114a4e5ee30adcbebab1ee0c1f555121345b9bb5
BetaBond <bond.ryo.beta@gmail.com>
2023-06-29 16:55:55 +0800

    fix: init

---
+++ app/Models/Trace/BookTrace.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Models\Trace;
+
+use Bond\Ryo\Trace\TraceEloquent;
+
+/**
+ * 追踪类 (帮助 ied 更好的发现)
+ *
+ * @author beta
+ */
+class BookTrace extends TraceEloquent
+{
+    
+    /**
+     * 表名称
+     *
+     * @var string
+     */
+    const TABLE = 'book';
+    
+    /**
+     * 账单 ID
+     *
+     * @var string
+     */
+    const ID = 'id';
+    
+    /**
+     * 账单类型
+     *
+     * @var string
+     */
+    const TYPE = 'type';
+    
+    /**
+     * Telegram UID
+     *
+     * @var string
+     */
+    const T_UID = 't_uid';
+    
+    /**
+     * 机器人 ID
+     *
+     * @var string
+     */
+    const ROBOT_ID = 'robot_id';
+    
+    /**
+     * 用户名
+     *
+     * @var string
+     */
+    const USERNAME = 'username';
+    
+    /**
+     * 金额
+     *
+     * @var string
+     */
+    const MONEY = 'money';
+    
+    /**
+     * 汇率
+     *
+     * @var string
+     */
+    const EXCHANGE_RATE = 'exchange_rate';
+    
+    /**
+     * 费率
+     *
+     * @var string
+     */
+    const RATE = 'rate';
+    
+    /**
+     * 创建时间
+     *
+     * @var string
+     */
+    const CREATED_AT = 'created_at';
+    
+    /**
+     * 更新时间
+     *
+     * @var string
+     */
+    const UPDATED_AT = 'updated_at';
+    
+    /**
+     * 隐藏列
+     *
+     * @var array
+     */
+    const HIDE = [];
+    
+}
