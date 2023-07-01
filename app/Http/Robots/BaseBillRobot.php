37bcc2702bf4aab76fda11374228b5547e26b634
BetaBond <bond.ryo.beta@gmail.com>
2023-07-01 17:39:22 +0800

    fix: init

--- app/Http/Robots/BaseBillRobot.php
+++ app/Http/Robots/BaseBillRobot.php
@@ -700,6 +700,9 @@ class BaseBillRobot
         }
         
         $messages = ["*当前欧易最优买卖价格：*"];
+        $messages[] = "[`银行卡`]: `￥7.2`";
+        $messages[] = "[`支付宝`]: `￥7.2`";
+        $messages[] = "[`微信`]: `￥7.2`";
         
         return implode("\n", $messages);
     }
